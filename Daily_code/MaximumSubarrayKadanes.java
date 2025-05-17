a93064a115badd47c432afce257cbf4b8314c11f
Suberna <141252637+Suberna@users.noreply.github.com>
2025-05-17 05:11:18 +0000

    add

---
+++ Daily_code/MaximumSubarrayKadanes.java
@@ -0,0 +1,19 @@
+/*Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6. */
+public class MaxSubarraySumKadanes {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = 0;
+
+        for (int num : nums) {
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+            if (currentSum < 0) currentSum = 0;
+        }
+
+        return maxSum;
+    }
+}
