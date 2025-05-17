be9cb636017344034e49ad728e0bbaeef7d13244
Suberna <141252637+Suberna@users.noreply.github.com>
2025-05-17 12:53:13 +0000

    add

---
+++ Daily_code/StockSellBuy.java
@@ -0,0 +1,16 @@
+class StockBuySell{
+    public int maxProfit(int[] prices) {
+        int minprices = prices[0];
+        int maxprofit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<minprices){
+                minprices = prices[i];
+            }else{
+                int profit = prices[i] - minprices;
+                maxprofit = Math.max(maxprofit,profit);
+            }
+        }
+        return maxprofit;
+        
+    }
+}
