033778d5753ccbb1b74f746b434a9b820480e83f
Suberna <141252637+Suberna@users.noreply.github.com>
2025-05-17 13:11:30 +0000

    add

---
+++ Daily_code/RearrangeArrayBySign.java
@@ -0,0 +1,29 @@
+/*Example 1:
+Input: nums = [3,1,-2,-5,2,-4]
+Output: [3,-2,1,-5,2,-4]
+Explanation:
+The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
+The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
+Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.   */
+
+public class RearrangeArrayBySign {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int pos = 0; // even indices for positives
+        int neg = 1; // odd indices for negatives
+
+        for (int num : nums) {
+            if (num > 0) {
+                result[pos] = num;
+                pos += 2;
+            } else {
+                result[neg] = num;
+                neg += 2;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Daily_code/RearrangeArraySigns.java
@@ -0,0 +1,22 @@
+
+public class RearrangeArrayBySign {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int pos = 0; // even indices for positives
+        int neg = 1; // odd indices for negatives
+
+        for (int num : nums) {
+            if (num > 0) {
+                result[pos] = num;
+                pos += 2;
+            } else {
+                result[neg] = num;
+                neg += 2;
+            }
+        }
+
+        return result;
+    }
+}
