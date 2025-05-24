8768bc218877119e51245f36f80568bb66d180e5
Suberna <141252637+Suberna@users.noreply.github.com>
2025-05-24 16:00:39 +0000

    add

---
+++ Daily_code/Strings/ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            // Swap characters at left and right
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            // Move pointers inward
+            left++;
+            right--;
+        }
+    }
+}
+
